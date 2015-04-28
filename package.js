90fa02ed7629e192d59634ed8ce14c8d931be333
Dave Allen <david.allen@meliortechnology.com>
2015-04-28 14:37:37 -0700

    Updated version.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'fullflavedave:paraview',
-    version: '0.0.1',
+    version: '0.0.2',
     summary: 'A user-friendly wrapper for the ParaViewWeb JavaScript API',
     git: 'https://github.com/fullflavedave/meteor-paraview.git',
     documentation: 'README.md'
